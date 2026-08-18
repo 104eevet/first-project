d91e3b5d5c2bfcd720fe60ef6ee8dd841aff9f18
ilya bocharov <bocharovilyawork@gmail.com>
2026-08-18 20:18:50 +0300

    change paths for images

--- src/scripts/skills-slider.js
+++ src/scripts/skills-slider.js
@@ -1,6 +1,6 @@
 const cardsData = [
   {
-    logo: "/icons/html-logo.svg",
+    logo: "/tech-icons/html-logo.svg",
     title: "HTML",
     description: `
       <p>Уверенно создаю структуру сайтов с помощью HTML5.</p>
@@ -8,22 +8,22 @@ const cardsData = [
     `,
   },
   {
-    logo: "/icons/sass-logo.svg",
+    logo: "/tech-icons/sass-logo.svg",
     title: "SASS",
     description: `
-      <p>Использую Sass, чтобы писать удобные и понятные стили.</p>
+      <p>Использую SCSS, чтобы писать удобные и понятные стили.</p>
       <p>Работаю с переменными, миксинами, вложенностью, функциями и разделением стилей на отдельные модули.</p>
     `,
   },
   {
-    logo: "/icons/react-logo.svg",
+    logo: "/tech-icons/react-logo.svg",
     title: "React",
     description: `
       <p>❌ Еще не знаю, но планирую изучить.</p>
     `,
   },
   {
-    logo: "/icons/js-logo.svg",
+    logo: "/tech-icons/js-logo.svg",
     title: "JavaScript",
     description: `
       <p>Уверенно использую JavaScript для создания интерактивности на сайтах.</p>
@@ -31,14 +31,14 @@ const cardsData = [
     `,
   },
   {
-    logo: "/icons/ts-logo.svg",
+    logo: "/tech-icons/ts-logo.svg",
     title: "TypeScript",
     description: `
       <p>❌ Еще не знаю, но планирую изучить.</p>
     `,
   },
   {
-    logo: "/icons/vite-logo.svg",
+    logo: "/tech-icons/vite-logo.svg",
     title: "Vite",
     description: `
       <p>Знаком с основами Vite и использую его для разработки фронтенд-проектов.</p>
@@ -46,7 +46,7 @@ const cardsData = [
     `,
   },
   {
-    logo: "/icons/git-logo.svg",
+    logo: "/tech-icons/git-logo.svg",
     title: "Git",
     description: `
       <p>Знаю основные команды Git и использую его для работы над проектами.</p>
@@ -54,7 +54,7 @@ const cardsData = [
     `,
   },
   {
-    logo: "/icons/tailwind-logo.svg",
+    logo: "/tech-icons/tailwind-logo.svg",
     title: "Tailwind",
     description: `
       <p>❌ Еще не знаю, но планирую изучить.</p>
